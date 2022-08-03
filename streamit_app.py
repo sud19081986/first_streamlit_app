278ec2fceec9ee72bb47b797e30222fdc0946562
sud19081986 <110534026+sud19081986@users.noreply.github.com>
2022-08-03 16:07:43 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -6,4 +6,6 @@ streamlit.text('1 Glass Milk')
 streamlit.text('🥑 Fruits')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index)
 streamlit.dataframe(my_fruit_list)
