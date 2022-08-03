203ab6b63dd6f16a5414c01f8ce419c332558df7
sud19081986 <110534026+sud19081986@users.noreply.github.com>
2022-08-03 16:01:46 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -3,5 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('2 Boiled Eggs ')
 streamlit.text('1 Glass Milk')
-streamlit.text('Fruits')
-🥣 🥗 🐔 🥑🍞
+streamlit.text('🥑 Fruits')
+
