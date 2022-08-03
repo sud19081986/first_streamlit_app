c2fe6e6094831e422efcd051dcda70d6d9612c5b
sud19081986 <110534026+sud19081986@users.noreply.github.com>
2022-08-03 16:03:56 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -4,4 +4,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('2 Boiled Eggs ')
 streamlit.text('1 Glass Milk')
 streamlit.text('🥑 Fruits')
-
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
