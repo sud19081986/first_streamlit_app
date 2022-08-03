76ac5446325ed9989491c883dfa16a22c70791f6
sud19081986 <110534026+sud19081986@users.noreply.github.com>
2022-08-03 16:14:13 +0530

    Update streamit_app.py

--- streamit_app.py
+++ streamit_app.py
@@ -7,6 +7,6 @@ streamlit.text('🥑 Fruits')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index)['Apple','Avacado'])
+fruits_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index)['Avocado'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
